37bb9654ec8a8dfea21264fe2e41e4ed3e417882
Vishal Patel <vpatel1215@gmail.com>
2019-11-18 11:02:48 -0600

    added marker

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const mapboxgl = require("mapbox-gl");
+//new mapboxgl.Marker(YOUR_DOM_ELEMENT).setLngLat(YOUR_COORDS_ARRAY).addTo(map);
+
+mapboxgl.accessToken = "pk.eyJ1IjoidnBhdGVsMTIxNSIsImEiOiJjazM0bnkzY28weHZ1M25tbTJmMHZrZTBiIn0.NgEC8rnV0Ql8T_S8T7dNuA";
+
+const map = new mapboxgl.Map({
+    container: "map",
+    center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+    zoom: 12, // starting zoom
+    style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+new mapboxgl.Marker().setLngLat([-87.6354, 41.8885]).addTo(map);
